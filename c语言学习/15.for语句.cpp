446086b1bb9fc446e9cb7a00eb3364ad4be581d6
pcw-jy <2659657040@qq.com>
2026-04-05 20:19:17 +0800

    Add files via upload

---
+++ c语言学习/15.for语句.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+  int score = 0;
+   // 1243243243... 直到2不成立，循环结束
+   // //1: 初始值 2: 循环条件 3: 结束值
+    // for (1; 2; 3);
+   // {
+   //     //代码体
+   //     printf("hello World\n");//4
+   // }
+   for (int a = 0; a < 100; a++)
+   {
+       printf("hello World\n");
+   }
+   return 0;
+
+
+    
+
+    return 0;
+}
---
+++ c语言学习/16.wfile以及dowhile.cpp
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int sum = 0;
+//	int i = 0;
+//	/*while (i <= 100)
+//	{
+//
+//		sum += i;
+//		i++;
+//
+//	}
+//	printf("%d\n", sum);*/
+//
+//	do 
+//	{
+//
+//		sum += i;
+//		i++;
+//
+//	} while (i <= 100);
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
