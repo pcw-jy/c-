89879797e938df3ba7948deb38e459b1461573f4
pcw-jy <2659657040@qq.com>
2026-05-16 12:19:43 +0800

    Add files via upload

---
+++ 46.动态内存申请之calloc.c
@@ -0,0 +1,14 @@
+// calloc: 申请内存空间，并往空间内存放数据(初始化)
+#include<stdio.h>
+#include<stdlib.h>
+// calloc在malloc的基础上多了一个初始化操作，系统会自动默认为0
+void main()
+{
+	int* p = (int*)colloc(10, sizeof(int));
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n", *(p + i));
+	}
+
+
+}
