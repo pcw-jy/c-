7b59852fdea97932ee80777c9dc6f1f641959993
pcw-jy <2659657040@qq.com>
2026-05-16 12:39:58 +0800

    Fix typo in calloc function call

--- 46.动态内存申请之calloc.c
+++ 46.动态内存申请之calloc.c
@@ -1,10 +1,10 @@
-// calloc: �����ڴ�ռ䣬�����ռ��ڴ������(��ʼ��)
+// calloc: 申请内存空间，并往空间内存放数据(初始化)
 #include<stdio.h>
 #include<stdlib.h>
-// calloc��malloc�Ļ����϶���һ����ʼ��������ϵͳ���Զ�Ĭ��Ϊ0
+// calloc在malloc的基础上多了一个初始化操作，系统会自动默认为0
 void main()
 {
-	int* p = (int*)colloc(10, sizeof(int));
+	int* p = (int*)calloc(10, sizeof(int));
 	for (int i = 0; i < 10; i++)
 	{
 		printf("%d\n", *(p + i));
