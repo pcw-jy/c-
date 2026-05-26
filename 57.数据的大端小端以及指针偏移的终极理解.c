ec76c8dff0ce3f0964130c71fca130349e7b7ae6
pcw-jy <2659657040@qq.com>
2026-05-26 15:32:25 +0800

    Add files via upload

---
+++ 57.数据的大端小端以及指针偏移的终极理解.c
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+void main()
+{	
+	//数据存储形式
+	//大端和小端存储
+	/*小端模式（Little-Endian）
+低字节放低地址，高字节放高地址
+日常电脑、手机、x86 CPU 全是小端
+​
+大端模式（Big-Endian）
+高字节放低地址，低字节放高地址
+网络协议、单片机、部分嵌入式常用*/
+	int m = 0xabcda0b0;
+	printf("%p\n", &m);
+	
+	short* pshort = (short*)&m;
+	printf("%x\n", *pshort);
+	printf("%p\n", &m);
+	printf("%p\n", pshort);
+	pshort++;
+	printf("%x\n", *pshort);
+	printf("%p\n", pshort);
+
+
+
+
+}
